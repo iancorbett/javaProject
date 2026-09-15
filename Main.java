bac71b4c6bf690a8261f05111fcfa6c14a2024cf
iancorbett <iancorbett1324@gmail.com>
2026-09-14 23:12:27 -0700

    update main file

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Korbski Labs has entered Java.");
+
+        Task task1 = new Task("Learn Java");
+        Task task2 = new Task("Call Nancy");
+
+        task1.display();
+        task2.display();
+
+        task1.complete();
+
+        task1.display();
     }
 }
